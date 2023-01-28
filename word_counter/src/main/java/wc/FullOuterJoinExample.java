48a260e139c94e294cf6de9f4578ad3c0c2d8a7a
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-01-28 19:10:27 +0200

    Develop Simple Word Counter and Run it on Apache Flink Execution Environment

---
+++ word_counter/src/main/java/wc/FullOuterJoinExample.java
@@ -0,0 +1,65 @@
+package wc;
+
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.operators.base.JoinOperatorBase;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.utils.ParameterTool;
+
+@SuppressWarnings("serial")
+public class FullOuterJoinExample
+{
+    public static void main(String[] args) throws Exception {
+
+        // set up the execution environment
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        final ParameterTool params = ParameterTool.fromArgs(args);
+        // make parameters available in the web interface
+
+        env.getConfig().setGlobalJobParameters(params);
+
+        // 1, Name
+        DataSet<Tuple2<Integer, String>> personSet = env.readTextFile(params.get("input1")).           //presonSet = tuple of (1  John)
+                map(new TextFilter());
+
+        DataSet<Tuple2<Integer, String>> locationSet = env.readTextFile(params.get("input2")).
+                map(new TextFilter());
+
+        // right outer join datasets on person_id
+        // joined format will be <id, person_name, state>
+
+        DataSet<Tuple3<Integer, String, String>> joined = personSet.fullOuterJoin(locationSet, JoinOperatorBase.JoinHint.OPTIMIZER_CHOOSES).where(0) .equalTo(0)
+                .with(new FullOuterJoin());
+        joined.writeAsCsv(params.get("output"), "\n", " ");
+
+        env.execute("Right Outer Join Example");
+    }
+    public static final class TextFilter implements MapFunction<String, Tuple2<Integer, String>>{
+        public Tuple2<Integer, String> map(String value)
+        {
+            String[] words = value.split(",");                                                 // words = [ {1} {John}]
+            return new Tuple2<Integer, String>(Integer.parseInt(words[0]), words[1]);
+        }
+    }
+
+    public static final class FullOuterJoin implements JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>> {
+        public Tuple3<Integer, String, String> join(Tuple2<Integer, String> person,  Tuple2<Integer, String> location){
+            // check for nulls
+            if (location == null)
+            {
+                return new Tuple3<Integer, String, String>(person.f0, person.f1, "NULL");
+            }
+            // for rightOuterJoin
+            else if (person == null)
+                return new Tuple3<Integer, String, String>(location.f0, "NULL", location.f1);
+
+            return new Tuple3<Integer, String, String>(person.f0,   person.f1,  location.f1);
+        }
+    }
+
+}
+
